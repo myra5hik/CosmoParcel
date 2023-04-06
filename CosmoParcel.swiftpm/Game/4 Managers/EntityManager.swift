3bed4104cc6510e9af18d903c4dd3cea6e2744dc
Alexander Tokarev <alexander.v.tokarev@gmail.com>
2023-04-06 17:25:44 +0200

    - Add: EntityManager

---
+++ CosmoParcel.swiftpm/Game/4 Managers/EntityManager.swift
@@ -0,0 +1,37 @@
+//
+//  File.swift
+//  
+//
+//  Created by Alexander Tokarev on 06.04.2023.
+//
+
+import Foundation
+import SpriteKit
+import GameplayKit
+
+final class EntityManager {
+    private var entities = Set<GKEntity>()
+    private let scene: SKScene
+
+    init(scene: SKScene) {
+        self.scene = scene
+    }
+
+    func add(entity: GKEntity) {
+        // Holds the reference
+        entities.insert(entity)
+        // Adds to the scene
+        if let node = entity.component(ofType: SpriteComponent.self)?.node {
+            scene.addChild(node)
+        }
+    }
+
+    func remove(entity: GKEntity) {
+        // Releases the entity
+        guard let removed = entities.remove(entity) else { return }
+        // Removes from the scene
+        if let node = removed.component(ofType: SpriteComponent.self)?.node {
+            node.removeFromParent()
+        }
+    }
+}
