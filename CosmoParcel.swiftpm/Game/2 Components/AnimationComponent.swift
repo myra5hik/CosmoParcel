9d7cc802fd082b1785a48a4a2328b735ffab2742
Alexander Tokarev <alexander.v.tokarev@gmail.com>
2023-04-07 02:02:00 +0200

    - Replaced Planet1 sprite atlas

--- CosmoParcel.swiftpm/Game/2 Components/AnimationComponent.swift
+++ CosmoParcel.swiftpm/Game/2 Components/AnimationComponent.swift
@@ -15,16 +15,21 @@ final class AnimationComponent: GKComponent {
     init(
         node: SKSpriteNode,
         atlas: SKTextureAtlas?,
-        fps: Double = 12
+        fps: Double = 24
     ) {
         self.node = node
+        // Frames derived from atlas
         self.frames = atlas?.textureNames.sorted(by: {
+            // If lengths differ (e.g., 'sprite-1' and 'sprite-11'), sorts by comparing lengths
             if $0.count < $1.count { return true }
             if $0.count > $1.count { return false }
+            // If lengths do not differ (e.g. 'sprite-1' and 'sprite-2'), sorts by comparing names
             return $0 < $1
         }).compactMap {
+            // Returns texture by its name in the bundle
             atlas?.textureNamed($0)
         } ?? []
+        // Fps
         self.fps = fps
         super.init()
         animate()
